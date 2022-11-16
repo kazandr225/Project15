97c93272f2cb8df148f265eb0fa9d92abc90fb31
Данил Казанский <danii.kazanskiy@mail.ru>
2022-11-16 14:02:25 +0300

    sd

--- Project1/Source.c
+++ Project1/Source.c
@@ -47,8 +47,24 @@ void tm(int cntrl) //часы
 		scanf_s("%d", &min);
 		print_s("Введите количество секунд");
 		scanf_s("%d", &sec);
+			
+		start = clock();
+		ns = start - end;
+		ms = ns * 10;
 		
-		start = clpck(0);
+		if (ms < 0)
+		{
+			sec = sec - 1;
+			ms = ms + 100;
+			end = start;
+		}
+		if (sec < 0)
+		{
+			min = min - 1;
+			sec = 0;
+		}
+
+		printf("%d:%d.%d\r", min, sec, ms);
 
 		break;
 	}
@@ -60,7 +76,7 @@ void control() //управление часами
 {
 	int command;
 	
-	printf_s(" 1 - запуск таймера;\n 2 - пауза;\n 3 - снять с паузы;\n 4 - режим секундомера\n");
+	printf_s(" 1 - режим секундомера;\n 2 - пауза;\n 3 - снять с паузы;\n 4 - запуск таймера\n");
 	//WaitForMultipleObjects(3, hThread, TRUE, INFINITE);
 	//Sleep(1000);
 
@@ -72,20 +88,20 @@ void control() //управление часами
 	switch (command)
 	{
 	case 1:
-		printf("Я заработал 1");
+		printf("запуск секундомера");
 		//hThread[1] = CreateThread(NULL, 0, time, NULL, 0, 0);
 		//WaitForMultipleObjects(3, hThread, TRUE, INFINITE);
 		break;
 	case 2:
-		printf("Я заработал 2");
+		printf("пауза");
 		SuspendThread(hThread[1]);
 		
 		break;
 	case 3:
-		printf("Я заработал 3");
+		printf("снять с паузы");
 		break;
 	case 4:
-		printf("Я заработал 4");
+		printf("режим таймера");
 		break;
 	}
 	
@@ -95,14 +111,22 @@ int main(int controlnum)
 {
 	system("chcp 1251>nul");
 	
-	//tm(1);
 	hThread[0] = CreateThread(NULL, 0, control, NULL, 0, 0);
 	hThread[1] = CreateThread(NULL, 0, time, NULL, 0, 0);
-	//control();
-	WaitForSingleObject(hThread, 100);
-
+	WaitForMultipleObjects(2, hThread,TRUE,INFINITE);
 	
+	int a[2];
+	GetExitCodeThread(hThread[0],a);
+	GetExitCodeThread(hThread[1], a+1);
+	printf("1 поток - %d, 2 поток - %d", *a, *(a+1));
 
+	for (size_t i = 0; i < 2 ; i++)
+	{
+		CloseHandle(hThread[i]);
+	}
 
-
+	return 0;
 }
+
+
+
